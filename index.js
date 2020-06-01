aa9379483c5a4c3ae7d4afb57f81cb473c5d0ffc
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-01 14:59:49 -0700

    provided files for assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs= require("fs");
+const inquirer= require("inquirer");
+//make sure all requires are at top of page//
+const axios= require("axios");
+const generateMarkdown= require("./utils/generateMarkdown");
+const path= require("path");
+
+//.gitignore will ignore what we tell it to//
+
+//add questions to user to an array of things we want to ask user//
+
+const questions = [
+{
+    type: "input",
+    message: "what is your user name for GitHub?"
+    name: "username",
+},
+{
+    type: "input",
+    message:
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
